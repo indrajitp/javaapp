3f24c3a16a1097d9e731d0413f1c45f22c1f409b
indrajitp <32815461+indrajitp@users.noreply.github.com>
2020-02-18 20:22:09 +0530

    Added init stage

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,7 +1,19 @@
 pipeline {
     agent any
-
+	tools
+    {
+        maven 'Jenkins-Maven'
+        jdk 'Jenkins-Java'
+    }
     stages {
+		stage ('Initialize'){
+            steps {
+                sh '''
+                    echo "PATH = ${PATH}"
+                    echo "M2_HOME = ${M2_HOME}"
+                '''
+            }
+        }
         stage('Build') {
             steps {
                 echo 'Building 2..'
