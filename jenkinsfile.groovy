bddf36f1bc8501ef27e09cdd8672ac0e4a4249a4
indrajitp <32815461+indrajitp@users.noreply.github.com>
2020-02-18 20:51:20 +0530

    changes

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -28,10 +28,37 @@ pipeline {
         }
         stage('upload') {
             steps {
-                sh '''
-                    echo "Need to add upload code here"
-                '''
+                def server = Artifactory.server 'artifactory'
+                def uploadSpec = """{
+                "files": [
+                    {
+                        "pattern": "$WORKSPACE/javaapp/*.war",
+                        "target": "libs-snapshot-local/com/gpi/gae/javaapp/SNAPSHOT/"
+                    }
+                ]
+                }"""
+                server.upload(uploadSpec)
+                def buildInfo = server.upload uploadSpec
+                server.publishBuildInfo buildInfo
             }
         }
+    }
+post {  
+         always {  
+             sh '''
+			 echo "Executing POST execution notification"
+			 '''  
+         }  
+         success {  
+             sh '''
+			 echo -e "Jenkins build compeleted for below services \n `cat ~/file_to_be_build` "|  mailx -r JekinsJobAlerts@globalpay.com -s "Jenkins CI SUCCESS" sanjeevkumarsingh@globalpay.com indrajit.patil3@globalpay.com
+			 '''  
+         }  
+         failure {  
+             sh '''
+			 echo -e "Jenkins build Failed for any of the below services \n `cat ~/file_to_be_build` "|  mailx -r JekinsJobAlerts@globalpay.com -s "Jenkins CI Failed" sanjeevkumarsingh@globalpay.com indrajit.patil3@globalpay.com
+			 '''
+         }  
+
     }
 }
