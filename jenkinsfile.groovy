b74a6e6e376f79529252d7d84b539f8293fc43ec
indrajitp <32815461+indrajitp@users.noreply.github.com>
2020-02-18 20:41:17 +0530

    Added build related changes

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -18,7 +18,6 @@ pipeline {
             steps {
 				sh '''
                 mvn clean install
-				
 				'''
             }
         }
@@ -29,20 +28,26 @@ pipeline {
         }
         stage('upload') {
             steps {
-                //Upload to artifactory
-                def server = Artifactory.server 'artifactory'
-                def uploadSpec = """{
-                "files": [
-                    {
-                        "pattern": "$WORKSPACE/javaapp/*.war",
-                        "target": "libs-snapshot-local/com/gpi/gae/${service}/${version}/"
-                    }
-                ]
-                }"""
-                server.upload(uploadSpec)
-                def buildInfo = server.upload uploadSpec
-                server.publishBuildInfo buildInfo
+                
             }
         }
+    }
+post {  
+         always {  
+             sh '''
+			 echo "Executing POST execution notification"
+			 '''  
+         }  
+         success {  
+             sh '''
+			 echo -e "Jenkins build compeleted for below services \n `cat ~/file_to_be_build` "|  mailx -r JekinsJobAlerts@globalpay.com -s "Jenkins CI SUCCESS" sanjeevkumarsingh@globalpay.com indrajit.patil3@globalpay.com
+			 '''  
+         }  
+         failure {  
+             sh '''
+			 echo -e "Jenkins build Failed for any of the below services \n `cat ~/file_to_be_build` "|  mailx -r JekinsJobAlerts@globalpay.com -s "Jenkins CI Failed" sanjeevkumarsingh@globalpay.com indrajit.patil3@globalpay.com
+			 '''
+         }  
+
     }
 }
