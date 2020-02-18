b7310f6b1773f16482e0a8eccc1b3993c9757399
indrajitp <32815461+indrajitp@users.noreply.github.com>
2020-02-18 21:35:48 +0530

    changes

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -32,7 +32,7 @@ pipeline {
 					sh '''
 					cp $WORKSPACE/target/*.war ./
 					'''
-						veracode applicationName: "App Engine Application", criticality: 'High', sandboxName: 'javaapp', pHost: 'analysiscenter.veracode.com', scanName: "javaapp-${BUILD_NUMBER}",uploadIncludesPattern: '*.*ar', useIDkey: true, vid: 'indrajit.patil3@globalpay.com', vkey: 'IP@Jan2020'
+						veracode applicationName: "App Engine Application", criticality: 'High', sandboxName: 'javaapp', pHost: 'analysiscenter.veracode.com', scanName: "javaapp-${BUILD_NUMBER}",uploadIncludesPattern: '*.*ar', useIDkey: true, vid: 'indrajit.patil3@globalpay.com', vkey: 'IP%Jan2020'
 				}
             }
         }
